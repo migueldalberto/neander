019b53483b370b9188c1cf07edf7ff891388206b
Miguel Dalberto Pedro <miguel.dalberto@inf.ufrgs.br>
2025-03-19 22:17:12 -0300

    add cli options using argp.h

--- main.c
+++ main.c
@@ -1,26 +1,82 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <argp.h>
 
 #include "neander.h"
 
+static char doc[] = "migueldalberto/neander - a simulator for NEANDER hypotetical computer";
+static char args_doc[] = "INPUT_MEMORY";
+
+static struct argp_option options[] = {
+  { "silent", 's', 0, 0, "Don't print memory to stdout", 0 },
+  { "debug", 'd', 0, 0, "Wait to run each instruction", 0 },
+  { "output", 'o', "FILE", 0, "Output memory (as bytes) to file", 0 },
+  { 0 }
+};
+
+#define N_OF_ARGS 1
+
+struct arguments {
+  char* args[N_OF_ARGS];
+  bool silent;
+  bool debug;
+  char* outputFile;
+};
+
+static error_t parse_opt (int key, char* arg, struct argp_state *state) {
+  struct arguments *arguments = state->input;
+
+  switch (key) {
+  case 's':
+    arguments->silent = true;
+    break;
+  case 'd':
+    arguments->debug = true;
+    break;
+  case 'o':
+    arguments->outputFile = arg;
+    break;
+  case ARGP_KEY_ARG:
+    if (state->arg_num >= N_OF_ARGS)
+      argp_usage(state);
+
+    arguments->args[state->arg_num] = arg;
+    break;
+  case ARGP_KEY_END:
+    if (state->arg_num < N_OF_ARGS)
+      argp_usage(state);
+    break;
+  default:
+    return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
 int main (int argc, char** argv) {
+  struct arguments arguments;
+
+  arguments.silent = false;
+  arguments.debug = false;
+  arguments.outputFile = "";
+
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  printf("ARGUMENT='%s', silent=%d, debug=%d, outputFile='%s'\n",
+	 arguments.args[0], arguments.silent, arguments.debug, arguments.outputFile);
+  
   Neander n = {0};
 
-  if (argc == 1 || argc > 3) {
-    fprintf(stderr, "bad arguments\n");
-    fprintf(stderr, "usage: %s INPUT OUTPUT\n", argv[0]);
-    return 1;
-  }
-
-  if (loadMemory(&n, argv[1])) {
-    fprintf(stderr, "failed to read file '%s'\n", argv[1]);
+  if (loadMemory(&n, arguments.args[0])) {
+    fprintf(stderr, "failed to read file '%s'\n", arguments.args[0]);
   } 
    
   startNeander(&n);
 
-  if (argc == 3) {
-    if (dumpMemory(n, argv[2])) {
+  if (arguments.outputFile[0] != '\0') {
+    if (dumpMemory(n, arguments.outputFile)) {
       fprintf(stderr, "failed to dump memory to file '%s'\n", argv[2]);
     }
   }
